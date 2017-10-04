85d649f9feee889634819f2515752b29424f0fbd
egusa hiroaki <egusa.hiroaki@gmail.com>
2017-10-04 10:54:40 +0900

    heroのpropertyを全て表示するために、appComponentのtemplateを修正

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div><label>name: </label>{{hero.name}}</div>
+    `
 })
 export class AppComponent {
   title = 'Tour fo Heroes'
