29843396132aad38e13f563c7e796d3f55335d93
egusa hiroaki <egusa.hiroaki@gmail.com>
2017-10-04 16:36:21 +0900

    サブコンポーネントで変数名を修正

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -4,16 +4,18 @@ import { Hero } from './hero';
 @Component({
   selector: 'hero-detail',
   template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
+    <div *ngIf="her">
+      <h2>{{her.name}} details!</h2>
+      <div><label>id: </label>{{her.id}}</div>
       <div>
         <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
+        <input [(ngModel)]="her.name" placeholder="name"/>
       </div>
     </div>
   `
 })
 export class HeroDetailComponent {
-  @Input() hero: Hero;
+  // @Inputデコレーションは、親のtemplate、ここではapp.componentの
+  // <hero-detail>で定義した[]の値をそのまま使用できる
+  @Input('hero') her: Hero;
 }
