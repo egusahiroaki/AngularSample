7a8e4e2750a4fa854ca42b8fa0d43fa08f1f0d77
egusa hiroaki <egusa.hiroaki@gmail.com>
2017-10-04 11:20:28 +0900

    dataとしてHEROESを用意し、listで表示

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
   template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <!--
     <div><label>id: </label>{{hero.id}}</div>
     <div>
       <label>name: </label>
       <input [(ngModel)]="hero.name" placeholder="name">
     </div>
+    -->
     `
 })
 export class AppComponent {
   title = 'Tour fo Heroes'
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  heroes = HEROES
 }
 
 // heroにpropertiesを追加。
@@ -27,3 +31,16 @@ export class Hero {
   id: number;
   name: string;
 }
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
